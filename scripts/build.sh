fdd0368cccc0a5c5946fb6b85cb08b297b495a1d
FigmentEngine <319349+FigmentEngine@users.noreply.github.com>
2025-11-11 11:03:34 +0000

    change license to .txt file on copy for mimetype

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ set -e
 sed "s#Last update:.*#Last update: $(date +%Y/%m/%d)#" src/humans.txt > src/humans.txt.tmp && mv src/humans.txt.tmp src/humans.txt
 
 # Copy LICENSE to src so mdBook includes it
-cp LICENSE src/LICENSE
+cp LICENSE src/LICENSE.txt
 
 # Install mdBook
 curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.4.40/mdbook-v0.4.40-x86_64-unknown-linux-gnu.tar.gz | tar -xz
--- theme/footer.js
+++ theme/footer.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	footer.style.paddingTop = '1rem';
 	footer.style.borderTop = '1px solid #ccc';
 	footer.style.textAlign = 'center';
-	footer.innerHTML = '<a href="/humans.txt">humans.txt</a> | <a href="/LICENSE">licence</a>';
+	footer.innerHTML = '<a href="/humans.txt">humans.txt</a> | <a href="/LICENSE.txt">licence.txt</a>';
 	content.appendChild(footer);
 
 	// Make content area flex to push footer down
