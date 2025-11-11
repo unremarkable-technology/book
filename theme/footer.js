35ca35e4f37c92f75d65284ac6a039142e068496
FigmentEngine <319349+FigmentEngine@users.noreply.github.com>
2025-11-11 11:42:02 +0000

    fixed us/uk spelling mismatch

--- theme/footer.js
+++ theme/footer.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	footer.style.paddingTop = '1rem';
 	footer.style.borderTop = '1px solid #ccc';
 	footer.style.textAlign = 'center';
-	footer.innerHTML = '<a href="/humans.txt">humans.txt</a> | <a href="/LICENSE.txt">licence.txt</a> | <a href="https://github.com/unremarkable-technology/book">source code</a>';
+	footer.innerHTML = '<a href="/humans.txt">humans.txt</a> | <a href="/LICENSE.txt">license.txt</a> | <a href="https://github.com/unremarkable-technology/book">source code</a>';
 	content.appendChild(footer);
 
 	// Make content area flex to push footer down
