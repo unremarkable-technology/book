68b7068d44448cdf85fdd56f7fb69d4af5cc9516
FigmentEngine <319349+FigmentEngine@users.noreply.github.com>
2026-03-24 16:13:58 +0000

    Start to automate output, first install intent

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,17 @@ cp LICENSE src/LICENSE.txt
 # Install mdBook
 curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.5.2/mdbook-v0.5.2-x86_64-unknown-linux-gnu.tar.gz | tar -xz
 
+# install intent
+mkdir -p "$HOME/.local/bin"
+export PATH="$HOME/.local/bin:$PATH"
+sh src/chapters/1/examples/install.sh
+
+# generate outputs
+find src/chapters -path '*/examples/*.sh' | while read -r script; do
+  out="${script%.sh}.out"
+  sh "$script" > "$out"
+done
+
 # Build book
 ./mdbook build -d public
 
